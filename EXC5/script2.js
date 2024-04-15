3886058acf9f8995b65cedc34b66224b585a6a05
SivanPesahov <sivanp27540@gmail.com>
2024-04-16 01:36:42 +0300

    16/04/24

--- EXC5/script2.js
+++ EXC5/script2.js
@@ -4,7 +4,11 @@ let adding_check = 0
 let subtracting_check = 0
 let multiplaying_check = 0
 let division_check = 0
-let global = 0
+
+let num1 = 0
+let num2 = 0
+let result = 0
+let global_sum = 0
 
 const elemAnswerLine = document.querySelector('#calc_window')
 
@@ -13,9 +17,32 @@ function insertNumber(num){
     elemAnswerLine.innerText = sum
 }
 
+function insertNumber2(n){
+    sum += n
+    num1 = parseFloat(sum)
+    elemAnswerLine.innerText = sum
+}
+
 function resetToZero(){
     sum = ""
     elemAnswerLine.innerText = '0'
+}
+
+function resetToZero2(){
+    num1 = 0
+    num2 = 0
+    result = 0
+    elemAnswerLine.innerText = '0'
+}
+
+function resetValues2(){
+    result = 0
+    num1 = 0
+    num2 = 0
+    adding_check = 0
+    subtracting_check = 0
+    multiplaying_check = 0
+    division_check = 0
 }
 
 function resetValues(){
@@ -61,13 +88,85 @@ function equals(){
 
 }
 
+function equals2(){
+    if (adding_check == 1){
+        result += num1
+        elemAnswerLine.innerText = result
+        resetValues2()
+    }
+    if (subtracting_check == 1){
+        result -= num1
+        elemAnswerLine.innerText = result
+        resetValues2()
+    }
+    if (multiplaying_check == 1){
+        result *= num1
+        elemAnswerLine.innerText = result
+        resetValues2()
+    }
+    if (division_check == 1){
+        result /= num1
+        elemAnswerLine.innerText = result
+        resetValues2()
+    }
+    
+}
+
+
+function adding2(){
+    sum =''
+    num2 = num1
+    result += num2
+    num1 = 0
+    adding_check = 1
+}
+
+function subbing2(){
+    sum =''
+    if (result > 0)
+    {
+        result -= num1
+    }
+    else{
+        result = num1
+    }
+    num1 = 0
+    subtracting_check = 1
+}
+
+function multi2(){
+    sum =''
+    if(result == 0)
+    {
+        result = num1
+    }
+    else{
+        result *= num1
+    }
+    num1 = 0
+    multiplaying_check = 1
+}
+
+function dividing2(){
+    sum =''
+    if (result == 0)
+    {
+        result = num1
+    }
+    else{
+        result /= num1
+    }
+    num1 = 0
+    division_check = 1
+}
+
+
+
 function adding(){
     if(adding_check ==1)
     {
         sum = parseFloat(sum) + int_sum
-        global += sum
         console.log(global)
-        global = 0
         savingToValues()
         adding_check = 1
     }
@@ -77,12 +176,12 @@ function adding(){
     }
 }
 
+
+
 function subbing(){
     if(subtracting_check == 1){
         sum = int_sum - parseFloat(sum) 
-        global -= sum
         console.log(global)
-        global = 0
 
         savingToValues()
         subtracting_check = 1
@@ -99,9 +198,7 @@ function subbing(){
 function multi(){
     if(multiplaying_check ==1){
         sum = parseFloat(sum) * int_sum
-        global *= sum
         console.log(global)
-        global = 0
 
         savingToValues()
         multiplaying_check = 1
@@ -117,9 +214,7 @@ function dividing(){
     if (division_check ==1)
     {
         sum = int_sum / parseFloat(sum) 
-        global /= sum
         console.log(global)
-        global = 0
 
         savingToValues()
         division_check = 1
