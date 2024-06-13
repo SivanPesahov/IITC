a350f4ad185d35b93fd2f4d471e7481a51af7f08
SivanPesahov <sivanp27540@gmail.com>
2024-06-13 23:18:33 +0300

    13.06.24

--- ReactTestProject/TodoList/src/Filter.jsx
+++ ReactTestProject/TodoList/src/Filter.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useMemo } from "react";
 
 function Filter(props){
   return(
-    <div className="message" >
+    <div className="filter-container" >
       <input type="search" placeholder="search here for a task" value={props.query} onChange={(ev) => props.setQuery(ev.target.value)}/>
       <select onChange={props.handleFilterChange}>
         <option value="all">Present all</option>
