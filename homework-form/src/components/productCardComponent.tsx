b951c52916d6495e250a23d328d2703be22f3d92
SivanPesahov <sivanp27540@gmail.com>
2024-08-04 18:02:53 +0300

    init

--- homework-form/src/components/productCardComponent.tsx
+++ homework-form/src/components/productCardComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
@@ -18,6 +18,8 @@ export interface Product {
 
 interface ProductCardProps extends Product {
   addToCart: (product: Product) => void;
+  removeProductFromCart: (productName: string) => void;
+  cart: Product[];
 }
 
 export const ProductCardComponent: React.FC<ProductCardProps> = ({
@@ -26,9 +28,25 @@ export const ProductCardComponent: React.FC<ProductCardProps> = ({
   price,
   image,
   addToCart,
+  removeProductFromCart,
+  cart,
 }) => {
+  const [added, setAdded] = useState(false);
+
   const handleAddToCart = () => {
     addToCart({ name, category, price, image });
+    setAdded(true);
+  };
+
+  const handleRemoveFromCart = () => {
+    removeProductFromCart(name);
+    const x = checkHowMany(name);
+    x === 1 ? setAdded(false) : null;
+  };
+
+  const checkHowMany = (productName: string) => {
+    const productInCart = cart.find((item) => item.name === productName);
+    return productInCart ? productInCart.quantity : 0;
   };
 
   return (
@@ -39,14 +57,36 @@ export const ProductCardComponent: React.FC<ProductCardProps> = ({
           alt={name}
           className="w-full h-60 rounded-lg"
         />
-        <div className="absolute inset-x-0 bottom-0 flex justify-center">
-          <Button
-            variant={"outline"}
-            className="rounded-full"
-            onClick={handleAddToCart}
-          >
-            <p className="mx-4">Add to Cart</p>
-          </Button>
+        <div className="absolute inset-x-0 bottom-0 flex justify-center items-center">
+          {added ? (
+            <>
+              <div className="flex items-center bg-orange-700 rounded-full">
+                <Button
+                  variant={"ghost"}
+                  className="rounded-full"
+                  onClick={handleRemoveFromCart}
+                >
+                  -
+                </Button>
+                <p className="mx-4">{checkHowMany(name)}</p>
+                <Button
+                  variant={"ghost"}
+                  className="rounded-full"
+                  onClick={handleAddToCart}
+                >
+                  +
+                </Button>
+              </div>
+            </>
+          ) : (
+            <Button
+              variant={"outline"}
+              className="rounded-full"
+              onClick={handleAddToCart}
+            >
+              <p className="mx-4">Add to cart</p>
+            </Button>
+          )}
         </div>
       </CardHeader>
       <CardContent>
